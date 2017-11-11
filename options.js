a17bc08008c7610572a401bc70c8339a43c9651d
ericywong <ericwong26@gmail.com>
2017-11-11 16:58:15 -0500

    added jQuery and linking API

--- options.js
+++ options.js
@@ -1,11 +1,25 @@
-chrome.storage.local.get('words', function (result) {
-  words = result.words;
-  words = ["ribosome", "dna", "something"];
-  for(i = 0; i < words.length; i++ ) {
-    button = document.createElement('button');
-    button.innerHTML = words[i];
-    document.getElementsByTagName('body')[0].appendChild(button);
-    button.addEventListener ("click", function() {
-           alert("button is clicked") });
-    };
-});
+function knowledgeGraph() {
+  var service_url = 'https://kgsearch.googleapis.com/v1/entities:search?query=taylor+swift&key=AIzaSyDTlx83dTXILreFQl8nkQfkvcvE3YI6rxc&limit=1&indent=True';
+  var params = {
+    'query': 'Taylor Swift',
+    'limit': 10,
+    'indent': true,
+    'key' : 'AIzaSyDTlx83dTXILreFQl8nkQfkvcvE3YI6rxc',
+  };
+
+  $.getJSON(service_url + '?callback=?', params, function(response) {
+    $.each(response.itemListElement, function(i, element) {
+      $('<div>', {text:element['result']['name']}).appendTo(document.body);
+    });
+  });
+}
+
+console.log(knowledgeGraph())
+  //
+  // words = ["ribosome", "dna", "something"];
+  // for(i = 0; i < words.length; i++ ) {
+  //   button = document.createElement('button');
+  //   button.innerHTML = words[i];
+  //   document.getElementsByTagName('body')[0].appendChild(button);
+  //   button.addEventListener ("click", knowledgeGraph);
+  // };
