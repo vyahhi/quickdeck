898271c55dd4840b76e1338d1725e8fc40210e48
Nikolay Vyahhi <vyahhi@gmail.com>
2017-11-11 15:02:04 -0500

    Launch button & local storage of words.

--- options.js
+++ options.js
@@ -1,10 +1,11 @@
-
-
-words = ["ribosome", "dna", "something"];
-for(i = 0; i < words.length+1; i++ ) {
-  button = document.createElement('button');
-  button.innerHTML = words[i];
-  document.getElementsByTagName('body')[0].appendChild(button);
-  button.addEventListener ("click", function() {
+chrome.storage.local.get('words', function (result) {
+  words = result.words;
+  words = ["ribosome", "dna", "something"];
+  for(i = 0; i < words.length; i++ ) {
+    button = document.createElement('button');
+    button.innerHTML = words[i];
+    document.getElementsByTagName('body')[0].appendChild(button);
+    button.addEventListener ("click", function() {
            alert("button is clicked") });
-  };
+    };
+});
